3f5c63e34de0a662bf633c5077541a050962ca5c
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-04 11:23:25 +0600

    _13-admin logout option_#3

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,10 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+
+
 });// end group admin middleware
 
 Route::middleware(['auth','role:agent'])->group(function(){
